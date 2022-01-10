c3b927661822564a0c1bc9b38aa6e7e8f2eec76c
Regijur <regijur.regis@gmail.com>
2022-01-10 03:47:50 -0300

    API USED

--- public/index.js
+++ public/index.js
@@ -1,7 +1,30 @@
-function doFetch(url) {
-    fetch(url).then(res =>{
-        if(res.ok) return res.json()
-    })   
-}
+const key = process.env.ITCHIO_KEY
 
-fetch("https://itch.io/api/1/jYczBr5szfluohoE2pa3718apIHiU7p7EZCTE8kX/my-games").then(res => {console.log(res)})
+let mygames = fetch(`https://itch.io/api/1/${key}/my-games`)
+    .then(res => res.json())
+    .then(res => res.games)
+
+mygames.then(e => {
+    e.map((e,i) => { MakeCardGames(e,i) })
+})
+
+function MakeCardGames(gameInfo, index) {
+    let bannerLink = gameInfo.cover_url
+    let gameName = gameInfo.title
+    let gameDescription = gameInfo.short_text
+    let gameItchLink = gameInfo.url
+
+    if(index == 0){
+        document.getElementsByClassName("gameContainer")[0].innerHTML = ""    
+    }
+
+    document.getElementsByClassName("gameContainer")[0].innerHTML += `<div class="cardGame">
+    <img class="gameBanner" src=${bannerLink}
+        alt="Banner do Jogo">
+    <div class="gameInfo">
+        <h2 class="gameName">${gameName}</h2>
+        <p class="gameDescription"><strong>Descrição:</strong> ${gameDescription} </p>
+        <a href=${gameItchLink} target="_blank">Clique para jogar</a>
+    </div>
+</div>`
+}
